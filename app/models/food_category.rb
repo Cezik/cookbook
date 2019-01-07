c9980111df787fa67a564b8ae13a135e88641ee9
Cezik <czepek221@gmail.com>
2019-01-07 11:17:25 +0100

    Little fix

--- app/models/food_category.rb
+++ app/models/food_category.rb
@@ -3,13 +3,16 @@ class FoodCategory < ApplicationRecord
 
   validates :title, presence: true
   validates :title, uniqueness: true
+  validates :title, length: { minimum: 5 }
 
   validate :capitalize_fist_char, if: :title
 
   private
 
   def capitalize_fist_char
-    title[0] = title[0].capitalize
+    if title.length > 0
+      title[0] = title[0].capitalize
+    end
     title
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,13 +2,17 @@ class Recipe < ApplicationRecord
   belongs_to :food_category
 
   validates :title, :recipe_text, :difficult, :photo_path, :food_category_id, presence: true
+  validates :title, uniqueness: true
+  validates :title, length: { minimum: 5 }
 
   validate :capitalize_fist_char, if: :title
 
   private
 
   def capitalize_fist_char
-    title[0] = title[0].capitalize
+    if title.length > 0
+      title[0] = title[0].capitalize
+    end
     title
   end
 end
